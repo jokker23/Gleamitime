725b81bfcef25ed1c1d7eaaf58cd985556a89b59
jokker23 <the@jokker.org>
2021-12-21 19:29:26 -0700

    bug fix

--- script.js
+++ script.js
@@ -11,25 +11,17 @@ window.requestAnimFrame = (function() {
   const hour = document.getElementById("hour"),
     min = document.getElementById("min"),
     sec = document.getElementById("sec"),
-    cincoM = new Audio('Meteor.mp3');
+    cincoM = new Audio('Meteor.mp3'),
+    dateSTART = new Date("Dec 20, 2021 22:14:06 MST");
   var howled = false;
   (function loop() {
-    requestAnimFrame(loop);
-    draw();
-  })();
-
-  function draw() {
     var dateNOW = new Date();
-    const dateSTART = new Date("Dec 20, 2021 22:14:06 MST");
     dateSTART.setMinutes(dateSTART.getMinutes() - 60);
-    var dSeconds = Math.abs(dateNOW - dateSTART) / 1000;
+    var dSeconds = (dateNOW - dateSTART) / 1000;
     h = (dSeconds % 5400) / 60;
     m = dSeconds % 90;
     s = (m * 90);
     //console.log(Math.round(h)+":"+Math.round(m)+":"+Math.round(s));
-    hour.style.webkitTransform = "rotate(" + (h * 4) + "deg)";
-    min.style.webkitTransform = "rotate(" + (m * 4) + "deg)";
-    //sec.style.webkitTransform = "rotate(" + (s*4) + "deg)";
     if (Math.round(m) == 0) {
       if (Math.round(s) <= 10) console.log(h + ":" + m + ":" + s);
       if (!howled && Math.round(m) == 55) {
@@ -39,5 +31,12 @@ window.requestAnimFrame = (function() {
     } else {
       howled = false;
     }
+    requestAnimFrame(loop);
+    draw(h,m);
+  })();
+
+  function draw(h,m) {    
+    hour.style.webkitTransform = "rotate(" + (h * 4) + "deg)";
+    min.style.webkitTransform = "rotate(" + (m * 4) + "deg)";    
   }
 })();
